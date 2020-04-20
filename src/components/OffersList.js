edf4fc384ad744270955e83d82bb090e145816c9
Dusty <dustymccord@gmail.com>
2020-04-20 16:16:45 -0700

    update lines.
    Co-authored-by: Rachel Schieferstein <violenza@gmail.com>
    Co-authored-by: Miles Bradt <smbradtmichael@gmail.com>

--- src/components/OffersList.js
+++ src/components/OffersList.js
@@ -4,7 +4,7 @@ import Offer from "./Offer";
 function OfferList() {
   return (
     <div className="offers">
-      <h3 className="offerUp"> We know things are bad but we still need your money.</h3>
+      <h3 className="offerUp"> We know things are bad but we still need your money. We really need your money.</h3>
       <div className="row">
         <div className="col-md-4">
           <Offer keyWordFear="fear" />
