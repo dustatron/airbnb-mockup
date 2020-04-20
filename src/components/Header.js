95cbd26e5024841a0f5c742b81a85d68c186a3ab
Dusty <dustymccord@gmail.com>
2020-04-20 15:29:08 -0700

    add header.
    Co-authored-by: Rachel Schieferstein <violenza@gmail.com>
    Co-authored-by: Miles Bradt <smbradtmichael@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,44 @@
 import React from "react";
+import logo from "../img/compass.png";
 
 function Header() {
   return (
     <React.Fragment>
-      <h1> Header </h1>;
+      <nav className="navbar navbar-expand-lg navbar-light ">
+        <a className="navbar-brand" href="/">
+          <img src={logo} className="logo" alt="logo" />where-bnb
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">
+                Host your home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Host an experience
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Help
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </React.Fragment>
   );
 }
